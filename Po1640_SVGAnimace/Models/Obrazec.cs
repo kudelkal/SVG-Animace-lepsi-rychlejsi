673b419d7671f3870d08bd5c42881823ae021d77
Lukáš Kudělka <kudelkal@office365.zcu.cz>
2024-05-06 18:13:51 +0200

    Přidejte soubory projektu.

---
+++ Po1640_SVGAnimace/Models/Obrazec.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+
+namespace Po1640_SVGAnimace.Models
+{
+    public abstract class Obrazec
+    {
+        public Obrazec(TypObrazce typObrazce, Color barva, int pozX, int pozY)
+        { 
+            TypObrazce = typObrazce;
+            Barva = barva;
+            PozX = pozX;
+            PozY = pozY;
+        }
+        public TypObrazce TypObrazce { get; set; }
+        public int PozX { get; set; }
+        public int PozY { get; set;}
+        public Color Barva { get; set; }
+
+        public void PosunObjekt(int krok, int SvgWidth, int SvgHeight, int MaxRozmer)
+        {
+            PozX += krok;
+            PozY += krok;
+
+            if (PozX > SvgWidth + MaxRozmer)
+            {
+                PozX = -MaxRozmer;
+            }
+            if (PozY > SvgHeight + MaxRozmer)
+            {
+                PozY = -MaxRozmer;
+            }
+        }
+
+    }
+
+
+   
+}
